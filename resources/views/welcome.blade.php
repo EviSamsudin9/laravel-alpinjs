2e5e2291bb6485a5943441728d069d767549dbeb
Evi Samsudin <evisamsudin44@gmail.com>
2023-03-08 15:45:18 +0700

    upload project1

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Cari Film...</title>
+        <!-- Tailwind play CDN -->
+        <script src="https://cdn.tailwindcss.com"></script>
+    </head>
+
+    <body>
+        <main class="py-10">
+            <div class="max-w-2xl mx-auto px-4">
+                <h1 class="text-center text-4xl font-bold text-gray-600">Cari Film...</h1>
+                <!-- Search form -->
+                <div class="my-10">
+                    <input type="search" class="px-4 py-2 block w-full border-2 border-gray-600 rounded-lg text-gray-600" placeholder="Ketik kata kunci di sini...">
+                </div>
+                <!-- End search form -->
+            </div>
+        </main>
+        <!-- Alpine.js CDN -->
+        <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.11.1/dist/cdn.min.js"></script>
+    </body>
+
+</html>
